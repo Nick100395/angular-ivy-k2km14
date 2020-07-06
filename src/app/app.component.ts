301db7129d51c1b93b66b9044994e5b5d0f3249f
Nick100395 <nisargushah@cmail.carleton.ca>
2020-07-06 14:49:14 -0400

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, VERSION } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-
 export interface experience_past {
   position: string;
   date_start: Date;
@@ -19,22 +18,30 @@ export interface experience_present {
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
+
 export class AppComponent  {
+
+  //String variable for Introduction part
   intro = 'Nisarg is a master’s graduate from Carleton University with a degree in Electrical and Computer Engineering. He has worked on several projects during his academic career including the development of an alumni website. He is passionate about working in a team and skilled in software development and programming. Nisarg believes that he has a strong ability to meet tight deadlines along with quality in work to accomplish desired goals. He has a keen interest in dealing with challenges which makes him an ideal software developer.';
 
+  //String variable for duties of TCS
   duties_TCS = 'Worked on the development of alumni website for Indus University in a team. Implemented efficient code with minimized number of lines, particularly with C# and C++. Identified and implemented areas of modification in existing program. Maintained and upgraded existing systems by making them more user friendly. Tested and evaluated new programs and fixed bugs/deficiencies';
 
+  //String variable for duties of FDM
   duties_FDM = 'Completed 16 weeks of training which includes SQL, UNIX, Eclipse, Git, JUnit, TDD, UML, OOP, Mockito, Maven, I/O, Multithreading, JDBC, JPA, Finance, HTML, CSS, JavaScript, JSP & Servlets, Spring Core, Spring MVC and Agile/Scrum. Worked on various projects such as interview feedback system, e-health care system, Tic-Tac-Toe game, user registration, etc. in Java. Followed scrum model for the group project and worked closely in a cross functional team to accomplish desired user stories/tasks for each sprint/iteration. Currently working to upscale AWS, Angular JS, JavaScript, and Node JS';
 
+  //Array for Educational Qualifications
   displayedColumns = ['Degree', 'University','Duration']
 
   dataSource = [{degree:'Bachelors', university:'Indus University', duration:'July 2012 - June 2016'}, {degree: 'Masters', university:'Carleton University',duration:'Jan 2017 - Dec 2018'}]
 
+  //Boolean variable for Academic Projects
   panel1OpenState = false;
   panel2OpenState = false;
   panel3OpenState = false;
   panel4OpenState = false;
 
+  //Arrays for Employment History
   experience_past: experience_past[] = [
     {
       position: 'Software Developer - Tata Consultancy Services, Ahmedabad, India',
@@ -50,14 +57,32 @@ export class AppComponent  {
     }
   ];
 
+  //Array for Interests & Activities
   members: {title: string, subtitle: string, content: string, url: string}[] = [
       {title: 'Tutoring', subtitle: 'Mathematics', content: 'Tutoring is my passion. I worked as a Math-tutor with high school students over a year and half. I enjoy helping students with complex problems and guiding them easiest methods.', url: 'https://cdn.jsdelivr.net/gh/Nick100395/angular-ivy-k2km14@master/src/Tutoring.jpg'},
       {title: 'Travelling', subtitle: 'Exploring World', content: 'Being an outdoor person, I love travelling and exploring new places quite often. I like talking to people of different cultures to know about their interests and lifestyle in detail.', url: 'https://cdn.jsdelivr.net/gh/Nick100395/angular-ivy-k2km14@master/src/Travelling.jpg'},
       {title: 'Puzzles', subtitle: 'Brainstorm', content: 'Since I prefer dealing with new challenges daily, I like solving complex puzzles where I can brainstorm and find the most efficient and fast way to get solution using my excellent problem solving ability.', url: 'https://cdn.jsdelivr.net/gh/Nick100395/angular-ivy-k2km14@master/src/Puzzle1.jpg'}
     ];
 
+  liked = 'LIKED';
 
+  //API Call from another Website
+  users = [];
+  apiUrl = 'https://jsonplaceholder.typicode.com/users';
 
-  
+  constructor(private http: HttpClient) {
+    this.http = http;
+  }
+
+  GetUserData() {
+    this.http.get<any[1]>(this.apiUrl)
+      .subscribe(data => {
+        this.users = data;
+      });
+  }
+
+  ClearUserData() {
+    this.users = [];
+  }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+//Material Design 
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatTableModule} from '@angular/material/table';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
-import {MatExpansionModule} from '@angular/material/expansion';
 import {MatMenuModule} from '@angular/material/menu';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatListModule} from '@angular/material/list';
 import {MatCardModule} from '@angular/material/card';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from '@angular/flex-layout';
 import {HttpClientModule} from '@angular/common/http';
 import {MatNativeDateModule} from '@angular/material/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
-import {FlexLayoutModule} from '@angular/flex-layout';
-
-
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
